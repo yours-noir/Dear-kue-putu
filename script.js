a665d5c2c3aa6993e90692071ce5cc8ed948b9d5
yours-noir <janteduhadam@gmail.com>
2026-09-19 14:31:01 +0700

    Fix selector case sensitivity in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function start() {
   document.body.classList.remove("not-loaded");
-  document.querySelector(".hah iy kelas gacor").style.display = "none";
+  document.querySelector(".Hah iy kelas gacor").style.display = "none";
 
   // Putar lagu
   const music = document.getElementById("bg-music");
