38697a4d36b436b9bab20d5133559fbc56d4b8e2
yours-noir <janteduhadam@gmail.com>
2026-09-20 06:27:19 +0700

    Replace start function with playMusic function

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function start() {
-  document.body.classList.remove("not-loaded");
-  document.querySelector(".Hah iy kelas gacor").style.display = "none";
-
-  // Putar lagu
-  const music = document.getElementById("bg-music");
-  music.volume = 0.7;
-  music.play();
+function playMusic() {
+  const lagu = document.getElementById("lagu");
+  lagu.volume = 0.8;
+  lagu.play();
 }
