ad7ed48d171de132cee29a27729d2906ccf3ac5d
yours-noir <janteduhadam@gmail.com>
2026-09-19 14:25:57 +0700

    Change display style for Tiptap element

---
+++ script.js
@@ -0,0 +1,9 @@
+function start() {
+  document.body.classList.remove("not-loaded");
+  document.querySelector(".hah iy kelas gacor").style.display = "none";
+
+  // Putar lagu
+  const music = document.getElementById("bg-music");
+  music.volume = 0.7;
+  music.play();
+}
